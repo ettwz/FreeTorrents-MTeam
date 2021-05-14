c1ff712ae71006d701f802720ed92317fd7e677e
Zhe Wang <ettwz@hotmail.com>
2021-05-14 16:28:55 +0800

    Add config file

--- main.go
+++ main.go
@@ -3,39 +3,28 @@ package main
 import (
 	"fmt"
 	"github.com/anaskhan96/soup"
+	"gopkg.in/yaml.v2"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 )
 
-var siteName = "M-TEAM"
+type Conf struct {
+	SiteCookie  string  `yaml:"siteCookie"`
+	PassKey     string  `yaml:"passKey"`
+	TorrentPath string  `yaml:"torrentPath"`
+	FreeDays    int     `yaml:"freeDays"`
+	FreeSize    float64 `yaml:"freeSize"`
+}
+
 var siteUrl = "https://tp.m-team.cc/torrents.php"
-var siteCookie = ""
-
-// It always would be the first part of your site, like: urlHalf = "https://tp.m-team.cc/"
-var urlHalf = "https://tp.m-team.cc/"
-
-// If your site is a Gazelle Site, please change this varible to True, like: isGazelle = True
-var isGazelle = false
-
-// If the torrents' download url in your site were encrypted like HDC, please change this varible to True, like: isEncrypted = True
-var isEncrypted = false
-
-// Check the download url, especially when you are using a https(SSL) url.
-// Some torrents' download pages url could be "https://tp.m-team.cc/download.php?id=xxxxx&https=1", in this case, you need to assign the variable of "url_last". Examples:
-// url_last = "&https=1"
-
-// If you couldn't downlaod the torrents to your directory where the *.py script is, you could just define the variables below. Make sure the format of your path because of the difference between Windows and Linux.
-var monitorPath = "/opt/freetorrent/torrent/"
-
-// Other informations for safer sites. Complete it if you cannot download torrents.
 var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
-
-// You don't need to define the variables shows below unless you couldn't download the torrents after defined the above one
 var referer = "https://tp.m-team.cc/login.php"
-var downloadURL = "https://tp.m-team.cc/download.php"
-var passKey = ""
+var downloadURL = "https://kp.m-team.cc/download.php"
 var host = "tp.m-team.cc"
 
 // You don't need to define the variables shows below unless you couldn't download the torrents after defined the above two
@@ -48,30 +37,6 @@ var cacheControl = ""
 var contentLength = ""
 var contentType = ""
 var origin = ""
-
-// Only if you just want to check the first 10 torrents is free in the page and download the free torrents in this small amount, please change it to 10
-// We always grab all the torrents in the whole page, but you can define the amount of grabing torrents by defining the variable below
-var torrentsAmount = 0
-
-// You don't need to change this variables unless you cannot download from your GAZELLE site
-// check this value from the page source code
-var colspan = "3"
-
-// You don't need to change this variables unless you cannot find free torrents correctly
-var freeTag = "pro_free"
-var freeTag2 = "pro_free2up"
-var DicFreeTag = "torrent_label tooltip tl_free"
-
-//PTP_free_tag = 'torrent-info__download-modifier--free'
-
-var torrentsClassName = ".torrentname"
-var HdcTorrentsClassName = ".t_name"
-var DicTorrentsClassName = ".td_info"
-
-//PTP_torrents_class_name = '.basic-movie-list__torrent-row--user-seeding'
-
-var downloadClassName = ".rowfollow"
-var HdcDownloadClassName = ".torrentdown_button"
 
 var headers = map[string]string{
 	"User-Agent":                userAgent,
@@ -88,15 +53,18 @@ var headers = map[string]string{
 	"content-type":              contentType,
 }
 
+var c Conf
+
 func main() {
 
+	c.getConf()
 	fetch()
 }
 
 func fetch() soup.Root {
 	fmt.Println("Fetch Url", siteUrl)
 	soup.Headers = setHeader()
-	soup.Cookie("tp", siteCookie)
+	soup.Cookie("tp", c.SiteCookie)
 
 	source, err := soup.Get(siteUrl)
 	if err != nil {
@@ -116,7 +84,7 @@ func fetch() soup.Root {
 				dateValue := date[1]
 				if strings.Contains(dateValue, "日") {
 					num, err := strconv.Atoi(dateValue[0:1])
-					if err != nil || num < 4 {
+					if err != nil || num < c.FreeDays {
 						continue
 					}
 				} else {
@@ -126,7 +94,7 @@ func fetch() soup.Root {
 
 			sizeStr := tr.FindNextSibling().FindNextSibling().FindNextSibling().Pointer.FirstChild.Data
 			size, err := strconv.ParseFloat(sizeStr, 32)
-			if err != nil || size > 150 {
+			if err != nil || size > c.FreeSize {
 				continue
 			}
 
@@ -139,12 +107,15 @@ func fetch() soup.Root {
 	for _, detail := range res {
 		tmp := strings.Split(detail, "?")
 		tmp1 := strings.Split(tmp[1], "&")
+		id := strings.Split(tmp1[0], "=")
 
-		downloadURL := downloadURL + "?" + tmp1[0] + "&passkey=" + passKey + "&https=1"
-		http.ServeFile()
-
+		downloadURL := downloadURL + "?" + tmp1[0] + "&passkey=" + c.PassKey + "&https=1"
+		err := DownloadFile(c.TorrentPath+"[M-TEAM]"+id[1]+".torrent", downloadURL)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Downloaded: " + downloadURL)
 	}
-
 	return doc
 }
 
@@ -156,4 +127,40 @@ func setHeader() map[string]string {
 		}
 	}
 	return res
+}
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func DownloadFile(filepath string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+func (c *Conf) getConf() *Conf {
+	yamlFile, err := ioutil.ReadFile("conf.yaml")
+	if err != nil {
+		log.Printf("yamlFile.Get err #%v", err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+	return c
 }
